54bf0a9a5d3785a666ec02dfc708d21461015276
batch2991 <56360778+batch2991@users.noreply.github.com>
2021-02-10 17:25:55 +0530

    ipchanged

--- addressbook_app/src/test/java/addressbook/Contacts.java
+++ addressbook_app/src/test/java/addressbook/Contacts.java
@@ -19,7 +19,7 @@ public class Contacts
 	  {
 		 driver = new FirefoxDriver();
 		 driver.manage().window().maximize();
-		 driver.get("http://54.145.136.153:7071/addressbook/");
+		 driver.get("http://54.224.151.132:7071/addressbook/");
 		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		 new Actions(driver).moveToElement(driver.findElement(By.xpath("//span[text()='New contact']"))).click().perform();
 		 
